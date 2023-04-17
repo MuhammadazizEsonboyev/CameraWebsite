a5ab17d43b01d35e858189837170bfa9cc53cca6
MuhammadazizEsonboyev <esonboyev03@bk.ru>
2023-04-17 17:37:40 +0500

    dix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect, useState } from 'react'
+import './index.css'
+import * as tf from '@tensorflow/tfjs'
+import * as facemash from '@tensorflow-models/face-landmarks-detection'
+import Webcam from 'react-webcam'
+import { drawMesh } from './utils'
 
-function App() {
+const App = () => {
+  const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
+  const [url, setUrl] = useState()
+
+  const handleClick = () => {
+    const imageSrc = webcamRef.current.getScreenshot()
+    setUrl(imageSrc)
+  }
+
+  const runFacemesh = async () => {
+    const net = await facemash.load(
+      facemash.SupportedPackages.mediapipeFacemesh
+    )
+    setInterval(() => {
+      detect(net)
+    }, 10)
+  }
+
+  const detect = async net => {
+    if (
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // get video properties
+      const video = webcamRef.current.video
+      const videoWidth = webcamRef.current.video.videoWidth
+      const videoHeight = webcamRef.current.video.videoHeight
+
+      // set video width
+      webcamRef.current.video.width = videoWidth
+      webcamRef.current.video.height = videoHeight
+
+      // set canvas width
+      canvasRef.current.width = videoWidth
+      canvasRef.current.height = videoHeight
+
+      // detection
+      const face = await net.estimateFaces({ input: video })
+      // ctx get from canvas
+      const ctx = canvasRef.current.getContext('2d')
+      requestAnimationFrame(() => {
+        drawMesh(face, ctx)
+      })
+    }
+  }
+
+  useEffect(() => {
+    runFacemesh()
+
+    // eslint-disable-next-line
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Webcam ref={webcamRef} className='webcam' />
+        <canvas ref={canvasRef} className='canvas' />
+        <button onClick={() => handleClick()} className='cameraBtn'>Click ScreenShot</button>
       </header>
+        <img src={url} alt='' className='imageScreen'/>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
